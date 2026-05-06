7660352f892dd648e248dd5bd81e78a1d8880260
onb6 <blierolivia@gmail.com>
2026-05-06 00:45:12 -0400

    init

---
+++ src/LandingPage.js
@@ -0,0 +1,34 @@
+import "./LandingPage.css";
+import MilesCard from "./MilesCard";
+
+const LandingPage = () => {
+  const items = [
+    {
+      title: "Olipop Ranking",
+      subtitle: "A page to keep track of our Olipop ranking!",
+      text: "Coming soooooon :)",
+      buttonText: "Let's Go!",
+      buttonDisabled: true,
+    },
+    {
+      title: "Another app",
+      subtitle: "Going to figure out more thingssss!",
+      text: "I love youuuuu",
+      buttonText: "Let's Go!",
+      buttonDisabled: true,
+    },
+  ];
+
+  return (
+    <div className="landing-page-container">
+      <h1>Hi Miles! I love you!</h1>
+      <div className="card-wrapper">
+        {items.map((item) => (
+          <MilesCard content={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ src/MilesCard.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button,
+} from "reactstrap";
+import "./MilesCard.css";
+
+const MilesCard = (content) => {
+  return (
+    <Card className="miles-card">
+      <img alt="Sample" src="https://picsum.photos/300/200" />
+      <CardBody>
+        <CardTitle className="miles-card-title" tag="h5">
+          {content.content.title}
+        </CardTitle>
+        <CardSubtitle className="mb-2 text-muted" tag="h6">
+          {content.content.subtitle}
+        </CardSubtitle>
+        <CardText>{content.content.text}</CardText>
+        <Button disabled={content.content.buttonDisabled}>
+          {content.content.buttonText}
+        </Button>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default MilesCard;
